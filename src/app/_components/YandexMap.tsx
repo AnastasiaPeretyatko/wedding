4a279f6f7d5c22c5a6723613efa8f33d67be5cb3
AnastasiaPeretyatko <aperetyatko989@gmail.com>
2025-08-26 18:13:40 +0300

    f

---
+++ src/app/_components/YandexMap.tsx
@@ -0,0 +1,84 @@
+const YandexMap = ({type}: {type: 'house' | "street"}) => {
+  return (
+    type === "house" ? (
+    <div style={{ position: "relative", overflow: "hidden", width: "100%" }}>
+      <a
+        href="https://yandex.ru/maps/971/taganrog/?utm_medium=mapframe&utm_source=maps"
+        style={{
+          color: "#eee",
+          fontSize: "12px",
+          position: "absolute",
+          top: 0,
+        }}
+      >
+        Таганрог
+      </a>
+      <a
+        href="https://yandex.ru/maps/971/taganrog/house/podgornaya_ulitsa_44/Z0EYfwRgTk0DQFptfX5zdHlhZw==/?ll=38.915700%2C47.225099&utm_medium=mapframe&utm_source=maps&z=14.59"
+        style={{
+          color: "#eee",
+          fontSize: "12px",
+          position: "absolute",
+          top: 14,
+        }}
+      >
+        Подгорная улица, 44 — Яндекс Карты
+      </a>
+      <iframe
+        src="https://yandex.ru/map-widget/v1/?ll=38.915700%2C47.225099&mode=search&ol=geo&ouri=ymapsbm1%3A%2F%2Fgeo%3Fdata%3DCgg1NzU5NTg1MBJm0KDQvtGB0YHQuNGPLCDQoNC-0YHRgtC-0LLRgdC60LDRjyDQvtCx0LvQsNGB0YLRjCwg0KLQsNCz0LDQvdGA0L7Qsywg0J_QvtC00LPQvtGA0L3QsNGPINGD0LvQuNGG0LAsIDQ0IgoNILkbQhXv5jxC&z=14.59"
+        width="100%"
+        height="400"
+        frameBorder="1"
+        allowFullScreen
+        style={{ position: "relative" }}
+      />
+     </div>
+    ) : (
+      <div style={{ position: "relative", overflow: "hidden", width: "100%" }}>
+      <a
+        href="https://yandex.ru/maps/org/otdel_zags_goroda_taganroga/177769129752/?utm_medium=mapframe&utm_source=maps"
+        style={{
+          color: "#eee",
+          fontSize: "12px",
+          position: "absolute",
+          top: "0px",
+        }}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        Отдел ЗАГС города Таганрога
+      </a>
+      <a
+        href="https://yandex.ru/maps/971/taganrog/category/registery_office/184107242/?utm_medium=mapframe&utm_source=maps"
+        style={{
+          color: "#eee",
+          fontSize: "12px",
+          position: "absolute",
+          top: "14px",
+        }}
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        ЗАГС в Таганроге
+      </a>
+      <iframe
+        src="https://yandex.ru/map-widget/v1/?display-text=%D0%97%D0%90%D0%93%D0%A1&ll=38.918414%2C47.215257&mode=search&oid=177769129752&ol=biz&sll=38.936699%2C47.208735&sspn=0.072784%2C0.090190&text=%7B%22text%22%3A%22%D0%97%D0%90%D0%93%D0%A1%22%2C%22what%22%3A%5B%7B%22attr_name%22%3A%22category_id%22%2C%22attr_values%22%3A%5B%22184107242%22%5D%7D%5D%7D&z=15"
+        width="560"
+        height="400"
+        frameBorder="1"
+        allowFullScreen
+        style={{ position: "relative" }}
+        title="Yandex Map - ЗАГС Таганрог"
+      />
+    </div>
+    )
+
+  )
+}
+
+export default YandexMap
+
+// style="box-sizing:border-box;text-decoration:none;color:#b3b3b3;font-size:10px;font-family:YS Text,sans-serif;padding:0 20px;position:absolute;bottom:8px;width:100%;text-align:center;left:0;overflow:hidden;text-overflow:ellipsis;display:block;max-height:14px;white-space:nowrap;padding:0 16px;box-sizing:border-box"
+
+// style="width:560px;height:800px;overflow:hidden;position:relative;"
+// style="width:100%;height:100%;border:1px solid #e6e6e6;border-radius:8px;box-sizing:border-box" 
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,95 +1,130 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import YandexMap from './_components/YandexMap';
+import styles from './page.module.css';
+import classNames from 'classnames';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.tsx</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const isLiving = true;
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
+  return (
+    <div className={styles.main}>
+
+      <div className={styles.photo}/>
+
+      <div className={styles.block}>
+          <h1>
+            <em className={classNames(styles.name, styles.green)}>Женя</em>
+            <em className={classNames(styles.plus, styles.green)}> + </em>
+            <em className={classNames(styles.name, styles.pink)}>Настя</em>
+          </h1>
+
+          <p>Мы будем счасливы разделить с вами радость неповторимого для нас дня &mdash; 
+            <br/>
+            дня нашей свадьбы!
+          </p>
+      </div>
+
+      <div className={styles.block}>
+        {/* Подумать на счет времени */}
+          <h2 className={styles.date}>19.09.2025 в 14:00</h2>
+
+            <div className={classNames(styles.calendar, {
+              [styles.living]: isLiving
+            })}>
+              <table className={styles.table}>
+                <thead>
+                  <tr>
+                    <th>Пн</th>
+                    <th>Вт</th>
+                    <th>Ср</th>
+                    <th>Чт</th>
+                    <th>Пт</th>
+                    <th>Сб</th>
+                    <th>Вс</th>
+                  </tr>
+                </thead>
+                <tbody> 
+                  <tr>
+                    <td>15</td>
+                    <td>16</td>
+                    <td>17</td>
+                    <td>18</td>
+                    <td>19</td>
+                    <td>20</td>
+                    <td>21</td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+
+
+      </div>
+
+      <div className={styles.block}>
+        <h1>Тайминг</h1>
+        <div>
+        <span style={{ fontSize: '1.5em', color: '#8f995f' }}>♥</span>
+        <h2 className={styles.time}>14:30</h2>
+        <p>Сбор гостей у ЗАГСа</p>
+
         </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
+        <div>
+        <span style={{ fontSize: '1.5em', color: '#8f995f' }}>♥</span>
+        <h2 className={styles.time}>15:00</h2>
+        <p>Церемония регистрации</p>
+        </div>
+
+      <div>
+
+        <span style={{ fontSize: '1.5em', color: '#8f995f' }}>♥</span>
+        <h2 className={styles.time}>17:30</h2>
+        <p>Банкет</p>
+      </div>
+
+
+      </div>
+
+      <div className={styles.block}>
+        <h1>Локация ЗАГСа</h1>
+        <p>Ростовская область, г. Таганрог, Александровская улица 92</p>
+        <YandexMap type='street'/>
+      </div>
+
+      <div className={styles.block}>
+        <h1>Локация банкета</h1>
+        <p>Ростовская область, г. Таганрог, Подгорная улица дом 44</p>
+        <YandexMap type='house'/>
+      </div>
+
+      <div className={styles.block}>
+        <h1>Детали</h1>
+        <span style={{ fontSize: '1.5em', color: '#8f995f' }}>♥</span>
+        <h2>Проживание</h2>
+        <p>Мы хотим хотим чтобы все гости чувствовали себя комфортно, поэтому для Вас будет организовано проживание в доме для продолжения празднования до воскресенья.</p>
+
+        <span style={{ fontSize: '1.5em', color: '#8f995f' }}>♥</span>
+        <h2>Развлечения</h2>
+        <p>После завершения официальной части мероприятия в нашем распоряжении бассейн и в пешей доступности море, возьмите всё!</p>
+
+        <span style={{ fontSize: '1.5em', color: '#8f995f' }}>♥</span>
+        <h2>Дресс-код</h2>
+        <p>Мы не хотим ограничивать Вас в цветовой гамме, поэтому выбирайте те образы, в которых будете чувствовавать себя на 100 бвллов!</p>
+
+        <span style={{ fontSize: '1.5em', color: '#8f995f' }}>♥</span>
+        <h2>Подарки</h2>
+            <p>Мы не хотим обременять вас выбором подарка, поэтому будем очень рады вкладу в бюджет нашей молодой семьи.</p>
+
+        <span style={{ fontSize: '1.5em', color: '#8f995f' }}>♥</span>
+        <h2>Пожелания</h2>
+        <p>Будем благодарны если вы воздержитесь от криков &quot;Горько!&quot; на празднике, ведь поцелуй - это знак выражения чувств, и он не может быть исполнен по заказу.</p>
+      </div>
+      
+
+      <div className={styles.photo}/>
+
+      <div className={styles.block}>
+        <h1 style={{color: '#8E0100'}}>До встречи!</h1>
+        <p>С любовью. Женя и Настя!</p>
+      </div>
     </div>
   );
 }
